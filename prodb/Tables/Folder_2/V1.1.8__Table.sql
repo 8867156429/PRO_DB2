e8e5b9667df94664a299eb78c7fdfdd84546351d
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-14 00:58:25 +0530

    Add files via upload

---
+++ prodb/Tables/Folder_2/V1.1.8__Table.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA DEMO_THREE;
+CREATE TABLE Third_table
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO second_table
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
